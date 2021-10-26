1303a065b25d3d52377047aa6774428b072de47b
JhonAlexanderRodriguezOrtega <pcnaranda@gmail.com>
2021-10-25 21:51:24 -0500

    Variables y tipos de datos 21-10-2021

---
+++ main.js
@@ -0,0 +1,52 @@
+// Que es una variableURL
+// https://desarrolloweb.com/articulos/508.php
+
+// Tipos de datosURL
+// https://www.netinetidesign.com/post/tipos-de-datos-en-javascript/
+
+// Datos String
+let nombre = 'Jhon Alexander Rodriguez Ortega';
+console.log(nombre);
+console.log(Object(nombre));
+
+// Dato Number
+let edad = 45;
+console.log(edad);
+console.log(Object(edad));
+
+// Dato Number
+let altura = 1.70;
+console.log(altura);
+console.log(Object(altura));
+
+// Dato boolean
+let soyProfesor = false;
+console.log(soyProfesor);
+console.log(Object(soyProfesor));
+
+// Datos null
+let pelo = null;
+console.log(pelo);
+console.log(Object(pelo));
+
+// Dato undefined
+let brazos; 
+console.log(brazos);
+console.log(Object(brazos));
+
+// Datos Array
+let pasatiempos = ["Musica", "Peliculas", "Viajes"];
+console.log(pasatiempos);
+
+// Datos Array
+let contacto = [];
+contacto['Celular'] = 3188101050;
+contacto['email'] = "pcnaranda@gmail.com";
+console.log(contacto);
+
+// Datos Objeto
+let dirrecion = {
+    calle: "37",
+    numero: "32-26",
+    departamento: "Santander"
+};
